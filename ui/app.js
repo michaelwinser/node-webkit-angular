c2086209b96a3ae5275fc383865feaf2a11376d6
Michael Winser <michaelw.net@gmail.com>
2013-07-01 20:56:54 -0400

    Removed alert.  Added list of bombs.

--- ui/app.js
+++ ui/app.js
@@ -2,9 +2,17 @@
   var app = angular.module("app", ["Bomb"]);
 
   app.controller("MainController", function($scope, Bomb) {
-    $scope.bomb = new Bomb(1000);
+    $scope.bombs = [];
+
+    $scope.explosions = 0;
+
+    $scope.add_bomb = function(delay) {
+      console.log("add_bomb(", delay, ")")
+      $scope.bombs.push(new Bomb(delay * 1000));
+    }
+
     $scope.$on("boom", function() {
-      alert("bomb exploded");
+      $scope.explosions++;
     });
   });
 })();
